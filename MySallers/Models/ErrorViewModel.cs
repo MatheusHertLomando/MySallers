602fce6f1bb2639e7510a463485fb62bc985ca29
MatheusHertLomando <gitmatheuslomando@gmail.com>
2023-07-04 14:48:49 -0400

    projeto criado

---
+++ MySallers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MySallers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
